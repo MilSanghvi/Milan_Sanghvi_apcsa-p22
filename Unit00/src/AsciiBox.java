2bd5c6174c2e84c23bda1128843ec16de5843d33
MilSanghvi <38955555+MilSanghvi@users.noreply.github.com>
2019-02-01 10:54:09 -0800

    Unit 00 files
    Unit00 files

---
+++ Unit00/src/AsciiBox.java
@@ -0,0 +1,33 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class AsciiBox
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Milan Sanghvi \t  1/31/19 \n\n" );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+	}
+}
