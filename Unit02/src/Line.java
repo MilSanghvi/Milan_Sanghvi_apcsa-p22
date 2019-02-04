24fb06901f63a0399975d7f393e9076d3ae9e22f
MilSanghvi <38955555+MilSanghvi@users.noreply.github.com>
2019-02-04 10:41:26 -0800

    Unit 2 fix

--- Unit02/src/Line.java
+++ Unit02/src/Line.java
@@ -7,7 +7,9 @@ public class Line
 {
 	public static double getSlope( float x1, float y1, float x2, float y2 )
 	{
-		return ((y2 - y1) / (x2 - x1));
+		double getSlope = (double)((y2 - y1) / (x2 - x1));
+		return getSlope;
 	}
+	
 
 }
