754ed3780b72cca7e1484cf64f9f35bfa16c288c
MilSanghvi <38955555+MilSanghvi@users.noreply.github.com>
2019-02-23 14:38:00 -0800

    Unit07 everything but trips
    Trips still to be done

---
+++ Unit07/src/RPSRunner.java
@@ -0,0 +1,34 @@
+//(c) A+ Computer Science
+// www.apluscompsci.com
+//Name -  Milan Sanghvi
+
+import java.util.Scanner;
+import static java.lang.System.*;
+
+public class RPSRunner
+{
+	public static void main(String args[])
+	{
+		Scanner keyboard = new Scanner(System.in);
+		char response;
+		
+		//add in a do while loop after you get the basics up and running
+		
+			String player = "";
+			do
+			{
+			out.print("Rock-Paper-Scissors - pick your weapon [R,P,S] :: ");
+			
+			//read in the player value
+			player = keyboard.nextLine();
+		
+			RockPaperScissors game = new RockPaperScissors(player);	
+			out.println(game);
+			response = keyboard.nextLine().charAt(0);
+			}
+			while(response == 'y');
+	}
+}
+
+
+
