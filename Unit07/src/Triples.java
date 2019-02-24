8702c47140b09ed4dac24f9946b0ad1ab6a57fca
MilSanghvi <38955555+MilSanghvi@users.noreply.github.com>
2019-02-24 13:06:40 -0800

    Unit07 done
    Time to study for quiz

--- Unit07/src/Triples.java
+++ Unit07/src/Triples.java
@@ -10,39 +10,46 @@ public class Triples
 
 	public Triples()
 	{
-		this(0);
+		setNum(0);
 	}
 
 	public Triples(int num)
 	{
-
-
+		setNum(num);
 	}
 
 	public void setNum(int num)
 	{
-
-
+		number = num;
 	}
 	
 	private int greatestCommonFactor(int a, int b, int c)
 	{
 		int max = 0;
-
-
-
-		return 1;
+		for(int i = 1; i <= a; i++)
+		{
+			if(a % i == 0 && b % i == 0 && c % i == 0)
+				max = i;
+		}
+		return max;
 	}
 
 	public String toString()
 	{
 		String output="";
-
-
-
-
-
-
+		for(int i = 1; i < number; i++) 
+		{
+			for(int j = i; j < number; j++) 
+			{
+				if(i % 2 == j % 2) j++;
+				for(int k = j; k < number; k++)
+				{
+					if(Math.pow((double)i, 2) + Math.pow((double)j, 2) == Math.pow((double)k, 2))
+						if(greatestCommonFactor(i, j, k) < 2)
+							output += i + " " + j + " " + k + "\n";
+				}
+			}
+		}
 		return output+"\n";
 	}
 }
--- Unit07/src/TriplesRunner.java
+++ Unit07/src/TriplesRunner.java
@@ -1,4 +1,4 @@
-//(c) A+ Computer Science
+///(c) A+ Computer Science
 // www.apluscompsci.com
 //Name -  
 
@@ -8,5 +8,7 @@ public class TriplesRunner
 {
    public static void main(String args[])
    {
+	   Triples test = new Triples(110);
+	   out.println(test);
 	}
 }
