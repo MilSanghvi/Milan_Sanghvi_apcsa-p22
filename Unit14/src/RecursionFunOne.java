5993836b9dcec0ccad84ef09c9eecf25466f0bfe
MilSanghvi <38955555+MilSanghvi@users.noreply.github.com>
2019-03-28 21:47:16 -0700

    Unit14 Complete
    Need to review code over weekend

--- Unit14/src/RecursionFunOne.java
+++ Unit14/src/RecursionFunOne.java
@@ -8,8 +8,11 @@ public class RecursionFunOne
 {
 	public static int countOddDigits(int num)
 	{
-
-
-		return 0;
+		if(num > 0)
+		{
+			if(num % 2 == 0) return 1 + countOddDigits(num/10);
+			else return 0 + countOddDigits(num/10);
+		}
+		else return 0;
 	}
 }
