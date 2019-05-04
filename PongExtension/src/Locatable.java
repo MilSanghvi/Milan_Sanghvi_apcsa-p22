f2403247b0facd290f3e3f995e655d9c1f26b634
MilSanghvi <38955555+MilSanghvi@users.noreply.github.com>
2019-05-03 22:19:35 -0700

    PongExtentionDone
    PongDone

---
+++ PongExtension/src/Locatable.java
@@ -0,0 +1,13 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+public interface Locatable
+{
+    public void setPos( int x, int y);
+    public void setXSpeed( int x );
+    public void setYSpeed( int y );
+
+    public int getX();
+    public int getY();
+}
